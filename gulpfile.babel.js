b231a08ced9c4beb9bbb59e92e897bc6586a4962
pidokige02 <pidokige0204@gmail.com>
2023-01-24 17:16:53 +0900

    #4.0 Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,10 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+// import sass from "gulp-sass";
+
+// sass.compiler = require("node-sass");
+const sass = require('gulp-sass')(require('sass'));
 
 
 const routes = {
@@ -15,6 +19,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/styles.scss",
+    dest: "build/css"
   }
 };
 
@@ -28,9 +37,8 @@ const clean = () => del(["build/"]);
 
 // export const dev = gulp.series([clean, pug]);
 // 위의 것은 test 를 위해 temp 로 만든 것임
-
 const webserver = () =>
-    gulp.src("build").pipe(ws({ livereload: true, open: true }));
+    gulp.src("build").pipe(ws({ livereload: true}));
 // build directory 아래의 것을 webserver 를 이용하여 loading 한다는 의미임.
 
 const img = () =>
@@ -39,15 +47,22 @@ const img = () =>
     .pipe(image())
     .pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+    gulp
+      .src(routes.scss.src)
+      .pipe(sass().on("error", sass.logError))
+      .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 // big image 같은 경우는 시간이 많이 걸리기 때문에 dev 준비 과정에서 처리한다.
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const live = gulp.parallel([webserver, watch]);
 
