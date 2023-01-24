9400e030aab7c0e25855389a3fa9c1474fbbefe4
pidokige02 <pidokige0204@gmail.com>
2023-01-24 15:23:11 +0900

    #3.0 Image Optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,18 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
+
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",  // src 아래 all directory의 pug file 을 들여다본다
     src: "src/*.pug",
     dest: "build"
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
   }
 };
 
@@ -27,16 +33,24 @@ const webserver = () =>
     gulp.src("build").pipe(ws({ livereload: true, open: true }));
 // build directory 아래의 것을 webserver 를 이용하여 loading 한다는 의미임.
 
+const img = () =>
+  gulp
+    .src(routes.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
-  gulp.watch(routes.pug.watch, pug); // 변화가 있으면 pug 를 실행하라.
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
+// big image 같은 경우는 시간이 많이 걸리기 때문에 dev 준비 과정에서 처리한다.
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
 // export const dev = gulp.series([prepare, assets]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
